9a1403e17f9f881371657f416779c1856e79e745
Omar Diaz <o_ma_rdiaz@hotmail.com>
2018-01-14 17:33:41 -0400

    change of strategy

--- CryptharithmsLogic.py
+++ CryptharithmsLogic.py
@@ -1,55 +1,57 @@
-# -*- coding: utf-8 -*-
-import os
-import sys
-import re
-from classes import *
+# Cryptarithmetic Solver -- Brute Force Method
+import random
 
-state = finishStatement
-info = infoProcessing
+# Using the brute force iterations of a random assortment of left side (before) letter values.
+def assignValues(letters):
+    num = len(letters)
+    possible = list(range(10))  # All values are [0,9]
+    count = 0
+    val = {"0" : 0}             # Creating a dicitonary for the letter/value combos
+    for each in letters:
+        pick = random.sample(possible,1)    # Each number can only be used once, so I sample then remove from the available pool.
+        possible.remove(pick[0])
+        val[each] = pick[0]
+        count += 1
+    return possible, val
 
-inputs = "inputs.txt"
-outputs = "outputs.txt"
+codeText = "WHAT + WAS + THY == CAUSE"      # Puzzle Text
 
-def initializeProgram():
-    global response
-    response = raw_input("INPUT A NAME FOR THE RESPONSE OR LEAVE BLANK FOR DEFAULT (response.txt) > ")
-    if response == "":
-        response = "response.txt"
-    solutionEngine()
+before = codeText.split(" == ")[0]          # String Play
+after = codeText.split(" == ")[1]
+beforeLetters = set("".join(before.split(" + ")))
+afterLetters = set(after)
 
-def solutionEngine():
-    info.extract(inputs)
-    contentFile = inputs
+beforeWords = before.split(" + ")
+afterLength = len(after)
 
-    for case in inputs:
-        #ex "WHAT + WAS + THY == CAUSE"
-        regex = re.compile('[^a-zA-Z ]')
-        regex.sub('', case)
+# Flip words
+wordAfter = after[::-1]                     # Set up the words so they are backwards so I can index in the typical direction.
+wordsBefore = [""]*len(beforeWords)
+for i, each in enumerate(beforeWords):
+    wordsBefore[i] = each[::-1]
+    zeros = afterLength - len(each)         # Add zeros to fill in empty places in higher places. "0" is in the dict as 0
+    wordsBefore[i] += "0"*zeros
 
-
-    info.extract(outputs)
-    contentFile = outputs
-
-    info.compare(solution, outputs)
-
-    #Writes a solution
-    responseFile(solution)
-    #Declares if the solution is ready
-    state.responseDone(state)
-
-def responseFile(solution):
-    fileResponse = open(response, "a+")
-    fileResponse.write(solution + '\n')
-    fileResponse.close
-
-def finished():
-    print("Solver finished, say 'data' to show the result, 'delete' to destroy the response file or 'exit'")
-    userR = raw_input("> ")
-    if userR == "data":
-        state.showResponse()
-    elif userR == "delete":
-        state.destroyTest()
-    elif userR == "exit":
-        sys.exit()
-
-initializeProgram()
+for iter in range(1000000):
+    available, Vals = assignValues(beforeLetters)   # Random Start
+    carry = 0                                       # No carry in first iteration (one's place)
+    for i in range(afterLength):
+        sum = carry
+        for j in range(len(beforeWords)):           # Add and carry
+            sum += Vals[wordsBefore[j][i]]
+        new = sum % 10
+        carry = sum // 10
+        if wordAfter[i] in Vals:                    # If we should know this result since the 'letter' exists in the before text.
+            if new != Vals[wordAfter[i]]:               # If we should know the value, check.
+                break
+        else:
+            if new in available:                    # if it's a new 'letter', can we assign it to a unique number value?
+                Vals[wordAfter[i]] = new
+                available.remove(new)
+                if i == afterLength - 1 and new != 0: # Have we reached it end?
+                    print("We Did it!!!")
+                    del Vals["0"]
+                    print(Vals)
+                    exit()
+            else:
+                break
